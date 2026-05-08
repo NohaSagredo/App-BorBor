43b76c2c38adfacd5aa5e4115c393c1610710c74
nrsagredo <r.p.sagredo@gmail.com>
2026-05-08 13:50:17 -0400

    Configurar deploy en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,7 @@ function AppContent() {
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AppContent />
       </BrowserRouter>
     </ThemeProvider>
